be8b034dd7da4231e5d0e8982e0bea70f4bc7291
Edwin Yovany <yovany.dev@gmail.com>
2023-12-09 09:13:06 -0500

    [BUILD] (Views): Creating and combining views

---
+++ SwiftUITest/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  SwiftUITest
+//
+//  Created by Edwin Yovany on 9/12/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 8)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
