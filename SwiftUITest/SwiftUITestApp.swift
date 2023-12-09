f1c10263b2ec848c5893845a3347c7f49db65810
Edwin Yovany <yovany.dev@gmail.com>
2023-12-09 08:21:25 -0500

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Edwin Yovany on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
