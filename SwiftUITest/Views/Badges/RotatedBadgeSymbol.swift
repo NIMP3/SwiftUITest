c1a532d8c2dfc5a6fae9de8555de52eca3db764b
Edwin Yovany <yovany.dev@gmail.com>
2024-01-15 22:50:46 -0500

    [BUILD] (Drawing and Animations): Implementing Paths - Shapes

---
+++ SwiftUITest/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUITest
+//
+//  Created by Edwin Yovany on 15/01/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
